d485ce6240d70f3502f3bbdd3294c581224c41a2
BSevault <b.sevault@gmail.com>
2022-01-10 14:27:15 +0100

    updates

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Getting Started with Create React App
+# Premier exercice React
+
+Ci dessous le contenu du readme généré automatiquement à l'initialisation du projet React.
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -3,12 +3,18 @@ import Button from '../../components/Button';
 
 const Todo = () => {
     
-    
-    
+    const todos = [
+        { id: 1, texte: "Todo 1"},
+        { id: 2, texte: "Todo 2"},
+        { id: 3, texte: "Todo 3"},
+        { id: 4, texte: "Todo 4"},
+    ]    
     return ( 
         <div className="todo">
-            <h1>Todo</h1>
-            <Button texte="Texte de test" />
+            <h1>Todos</h1>
+            <ul>{todos.map((todo) => <li>{todo.texte}</li> )}</ul>
+            
+           <Button texte="Texte de test" />
         </div>
      );
 }
